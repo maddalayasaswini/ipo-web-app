f652d723a960c90514c01b45c5a64b328bee44e3
AMIT KUMAR <126111875+Kumaramit0809@users.noreply.github.com>
2025-03-06 23:36:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+new Chart(document.getElementById('activeClientsChart'), {
+    type: 'line',
+    data: {
+        labels: ['2016-17', '2017-18', '2018-19', '2019-20', '2020-21', '2021-22', '2022-23', '2023-24'],
+        datasets: [{
+            label: 'Angel One',
+            data: [4, 5, 6, 6, 7, 18, 45, 50
